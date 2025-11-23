c5204a5d5a5e94f9db68fb0893a358b6e88cf419
russellmarine <166625806+russellmarine@users.noreply.github.com>
2025-11-23 17:26:01 -0500

    Update time-config.js

--- config/time-config.js
+++ config/time-config.js
@@ -1,16 +1,15 @@
 // Time zone config for RussellTV footer
 // Edit this file to add/remove locations.
-
 window.TIME_ZONES = [
-  { label: "Zulu",                        tz: "Etc/UTC" },
-  { label: "6th Comm - Brooklyn, NY",     tz: "America/New_York" },
-  { label: "Camp Lejeune, NC",            tz: "America/New_York" },
-  { label: "Camp Pendleton, CA",          tz: "America/Los_Angeles" },
-  { label: "Camp Smith, HI",              tz: "Pacific/Honolulu" },
-  { label: "Camp Foster, JP",             tz: "Asia/Tokyo" },
-  { label: "MARFORRES - New Orleans, LA", tz: "America/Chicago" },
-  { label: "Manama, BH",                  tz: "Asia/Bahrain" },
-  { label: "Stuttgart, DE",               tz: "Europe/Berlin" },
-  { label: "Naples, IT",                  tz: "Europe/Rome" },
-  { label: "Darwin, AU",                  tz: "Australia/Darwin" }
+  { label: "Zulu",                        tz: "Etc/UTC",              lat: 0.0 },
+  { label: "6th Comm - Brooklyn, NY",     tz: "America/New_York",     lat: 40.6782 },
+  { label: "Camp Lejeune, NC",            tz: "America/New_York",     lat: 34.7520 },
+  { label: "Camp Pendleton, CA",          tz: "America/Los_Angeles",  lat: 33.2153 },
+  { label: "Camp Smith, HI",              tz: "Pacific/Honolulu",     lat: 21.3891 },
+  { label: "Camp Foster, JP",             tz: "Asia/Tokyo",           lat: 26.3344 },
+  { label: "MARFORRES - New Orleans, LA", tz: "America/Chicago",      lat: 29.9511 },
+  { label: "Manama, BH",                  tz: "Asia/Bahrain",         lat: 26.2285 },
+  { label: "Stuttgart, DE",               tz: "Europe/Berlin",        lat: 48.7758 },
+  { label: "Naples, IT",                  tz: "Europe/Rome",          lat: 40.8518 },
+  { label: "Darwin, AU",                  tz: "Australia/Darwin",     lat: -12.4634 }
 ];
